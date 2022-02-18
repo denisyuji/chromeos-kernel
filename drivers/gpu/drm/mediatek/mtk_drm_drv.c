cb67e48073fd3e1e50cee2f0503dd1af8f13c70f
Guillaume Ranquet <granquet@baylibre.com>
2022-02-18 15:54:31 +0100

    drm/mediatek: dpi: Add dpintf support
    dpintf is the displayport interface hardware unit. This unit is similar
    to dpi and can reuse most of the code.
    
    This patch adds support for mt8195-dpintf to this dpi driver. Main
    differences are:
     - Some features/functional components are not available for dpintf
       which are now excluded from code execution once is_dpintf is set
     - dpintf can and needs to choose between different clockdividers based
       on the clockspeed. This is done by choosing a different clock parent.
     - There are two additional clocks that need to be managed. These are
       only set for dpintf and will be set to NULL if not supplied. The
       clk_* calls handle these as normal clocks then.
     - Some register contents differ slightly between the two components. To
       work around this I added register bits/masks with a DPINTF_ prefix
       and use them where different.
    
    Based on a separate driver for dpintf created by
    Jason-JH.Lin <jason-jh.lin@mediatek.com>.
    
    Signed-off-by: Markus Schneider-Pargmann <msp@baylibre.com>
    Signed-off-by: Guillaume Ranquet <granquet@baylibre.com>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -692,6 +692,8 @@ static const struct of_device_id mtk_ddp_comp_dt_ids[] = {
 	  .data = (void *)MTK_DPI },
 	{ .compatible = "mediatek,mt8183-dpi",
 	  .data = (void *)MTK_DPI },
+	{ .compatible = "mediatek,mt8195-dpintf",
+	  .data = (void *)MTK_DP_INTF },
 	{ .compatible = "mediatek,mt2701-dsi",
 	  .data = (void *)MTK_DSI },
 	{ .compatible = "mediatek,mt8173-dsi",
@@ -804,7 +806,8 @@ static int mtk_drm_probe(struct platform_device *pdev)
 		    comp_type == MTK_DISP_OVL_ADAPTOR ||
 		    comp_type == MTK_DISP_RDMA ||
 		    comp_type == MTK_DPI ||
-		    comp_type == MTK_DSI) {
+		    comp_type == MTK_DPI ||
+		    comp_type == MTK_DP_INTF) {
 			dev_info(dev, "Adding component match for %pOF\n",
 				 node);
 			drm_of_component_match_add(dev, &match, compare_of,
